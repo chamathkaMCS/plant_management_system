c22f88a1a315402869776531957ff408d9bab753
chamathkaMCS <chamathkamcs@gmail.com>
2025-03-06 15:43:16 +0530

    final

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-<h1 class="no4">all rights reserved @MCS_Designers contact - + 94 71 48 666 30</h1>
+<h1 class="no4">all rights reserved @MCS_Designers</h1>
 </div>
 </body>
 </html>
